e07c5bf52db7d0a34bd635589d1adc254f51e25f
dhambure98 <akila789liyanage@gmail.com>
2021-12-29 00:43:10 +0530

    'Hibernate_Institute'

---
+++ src/lk/SipsewanaInstitute/AppInitializer.java
@@ -0,0 +1,21 @@
+package lk.SipsewanaInstitute;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author : A.D.Liyanage
+ * @since : 0.1.0
+ **/
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
